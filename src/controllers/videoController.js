12b4f04865b4e109ee4bffdcc13a1d850cf8feeb
박중섭 <parkjungsub@bagjungseob-ui-MacBookAir.local>
2021-12-27 20:46:36 +0900

    search fix

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -235,7 +235,7 @@ export const search = async (req, res) => {
     if (keyword) {
         videos = await Video.find({
             title: {
-                $regex: new RegExp(`${keyword}$`, "i"),
+                $regex: new RegExp(`${keyword}`, "i"),
             },
         });
     }
